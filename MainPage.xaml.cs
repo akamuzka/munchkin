02b43b579db534c73f6b2d1b9a7a9025c06a290f
akamuzka <46309196+akamuzka@users.noreply.github.com>
2019-01-02 12:55:13 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("버튼눌림");
+        }
+    }
+}
